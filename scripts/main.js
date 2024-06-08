1ce0fcae4a7b363361c10d84a95948f152b87bcc
rudiko <chakryan_rudik@mail.ru>
2024-06-08 16:37:46 +0300

    add form and modyfied js code

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
-console.log("sda")
+const rangeNumber = document.getElementById('rangeNum');
+const inputRange = document.getElementById('rangeInput');
+const inputFile = document.querySelector('.input-file input[type=file]')
+const form = document.querySelector('.main__checkout-order-form')
+
+rangeNumber.textContent = inputRange.value + ' %';
+
+inputRange.addEventListener('input', (el) => {
+    rangeNumber.textContent = el.target.value + ' %';
+});
+
+inputFile.addEventListener('change', function() {
+    let file = this.files[0];
+    this.nextElementSibling.innerHTML = file.name;
+});
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let formData = new FormData(this);
+
+    fetch(this.action, {
+        method: 'POST',
+        body: formData
+    }).then(function(response) {
+        console.log(response);
+    }).catch(function(error) {
+        console.error(error);
+    });
+});
